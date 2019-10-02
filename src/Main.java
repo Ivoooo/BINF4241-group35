f5eb6a9761d7985423ab1e84d682643350f2c1e3
ivoooo <ivo.aeschlimann@hotmail.com>
2019-10-02 15:07:25 +0200

    Simple Version of Game Class

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,23 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Game gameboard = new Game(14);
+
+        System.out.println(gameboard.getTiles());
     }
 
+}
+
+class Game {
+    Game(int tile) {
+        this.tiles = tile;
+    }
+
+    int getTiles() {
+        return this.tiles;
+    }
+
+    int tiles;
 
 }
