2e699eaf7f683ef13e828a10f0de09e3f358df92
ivoooo <ivo.aeschlimann@hotmail.com>
2019-10-03 14:38:14 +0200

    Game class basic Square implementation

--- Project 1/src/Game.java
+++ Project 1/src/Game.java
@@ -1,9 +1,18 @@
 public class Game {
     private int size;
-    //private Player[] playerArray = new Player[amount]
+    //private Squares[] squareArray;
+    //private Player[] playerArray; = new Player[amount];
+
+
+
 
     public int setSize(int i) {
         this.size = i;
+        //squareArray = new Square[size];
+        //SquareArray[0].setStart;
+        //SquareArray[SquareArray.length() - 1].setEnd;
+        // ... define Ladders and Snackes
+
         return this.size;
     }
 
