eefba780e760277060ee0cc1d6086d2fc990fc48
LaraFr <34027454+LaraFr@users.noreply.github.com>
2019-10-03 14:02:15 +0200

    User interaction

--- Project 1/src/Main.java
+++ Project 1/src/Main.java
@@ -1,6 +1,25 @@
-public class Main {
+import java.util.Scanner;
+import java.util.HashMap;
 
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(" Gamesize: ");
+        Scanner keyboard = new Scanner(System.in);
+        int gamesize = keyboard.nextInt();
+        System.out.println("Your Game is of Size: " + gamesize);
+        System.out.println("How many players want to play? ");
+        int numplayers = keyboard.nextInt();
+
+        HashMap<String, String> Players = new HashMap<String, String>();
+        for (int i = 1 ; i <= numplayers;){
+            Players.put("Player"+i ,"currentsquare");
+            i++;
+        }
+        for (String i : Players.keySet()) {
+            System.out.println(i);
+        }
+
+
     }
 }
