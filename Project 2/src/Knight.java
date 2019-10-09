b93d99efd21ccb6566ae881d68a8f967b4a5a767
MarcoUZH <56075874+MarcoUZH@users.noreply.github.com>
2019-10-09 17:51:30 +0200

    Checkmove method in knight implemented

--- Project 2/src/Knight.java
+++ Project 2/src/Knight.java
@@ -1,7 +1,7 @@
 public class Knight extends Figur {
     private types type = types.knight;
     private colors col;
-    public Coordinates coordinate;
+    private Coordinates coordinate;
     public Knight(String color) {
         if(color.equals("white"))col = colors.white;
         else col = colors.black;
@@ -11,6 +11,12 @@ public class Knight extends Figur {
         int y;
         x = coordinate.getX();
         y = coordinate.getY();
-        
+        // x2 = coordinate.movingX();
+        // y2 = coordinate.movingY();
+        if (x2 - x = 1 || x2 - x = -1  && y2 -y = 2) return true; // Bewegung rechts oder links "diagnoal" nach oben
+        if(x2 - x = 2 ||x2 - x = -2 && y2 - y = 1) return true; // Bewegung links oder rechts 45 grad nach oben
+        if (x2 - x = 1 || x2 - x = -1 && y2- y = -2) return true; // Bewegung rechts oder links diagonal nach unten
+        if (x2 - x = 2 || x2 - x = -2 && y2- y = -1) return true;//Bewegung links oder rechts 45 grad nach unten
+        else return false;
     }
 }
