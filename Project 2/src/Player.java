b006d5185cfc074e1443cc2c69208b313ff72915
ivoooo <ivo.aeschlimann@hotmail.com>
2019-10-09 15:11:04 +0200

    Created basic Player class

---
+++ Project 2/src/Player.java
@@ -0,0 +1,17 @@
+public class Player {
+    private enum Color {
+        white,
+        black
+    }
+
+    private String name;
+    private Color col;
+
+    public Player(String name, String color) {
+        this.name = name;
+        if (color.equals("white")) col = Color.white;
+        else col = Color.black;
+    }
+
+
+}
