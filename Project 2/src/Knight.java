80764272853d1af4e72e95438659673877715412
MarcoUZH <56075874+MarcoUZH@users.noreply.github.com>
2019-10-09 15:45:22 +0200

    knight adjusted

--- Project 2/src/Knight.java
+++ Project 2/src/Knight.java
@@ -1,9 +1,13 @@
 public class Knight extends Figur {
     private types type = types.knight;
     private colors col;
-
+    public Coordinates coordinate;
     public Knight(String color) {
         if(color.equals("white"))col = colors.white;
         else col = colors.black;
+    }
+    public class checkmove(){
+        int coordinates;
+        // coordinates = cordinate.getnumber();
     }
 }
