339ce100f923bad1230cbd2b8276c384a1b8554a
ivoooo <ivo.aeschlimann@hotmail.com>
2019-10-16 13:53:11 +0200

    Initializer Chessboard()

--- Project 2/src/Chessboard.java
+++ Project 2/src/Chessboard.java
@@ -1,6 +1,29 @@
 public class Chessboard {
-    int[][] board = new int[8][8];
-    int[] grave = new int[32];
+    private Figur[][] board = new Figur[8][8];
+    private Figur[] grave = new Figur[32];
+
+    public Chessboard() {
+        String col = "white";
+        for(int i = 0, j; i < 16; ++i) {
+            if (i > 8) col = "black";
+            j = i % 8;
+            if (j == 0 || j == 7) board[0][j] = new Rook(col);
+            else if (j == 1 || j == 6) board[0][j] = new Knight(col);
+            else if (j == 2 || j == 5) board[0][j] = new Bishop(col);
+            else if (j == 3) board[0][j] = new Queen(col);
+            else board[0][j] = new King(col);
+        }
+
+        col = "white";
+        for(int i = 0; i < 8; ++i) {
+            board[1][i] = new Pawn(col);
+        }
+
+        col = "black";
+        for(int i = 0; i < 8; ++i) {
+            board[7][i] = new Pawn(col);
+        }
+    }
 
     boolean gameOver() {
         return false;
