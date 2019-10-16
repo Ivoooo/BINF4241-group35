e7db914a63081be6c1a6c5340ee91f66d5548a0a
LaraFr <34027454+LaraFr@users.noreply.github.com>
2019-10-16 12:46:27 +0200

    small changes

--- Project 2/src/Game.java
+++ Project 2/src/Game.java
@@ -1,6 +1,6 @@
 
 public class Game {
-
+    Chessboard ChessBoard = new Chessboard;
 
   /*  public void addPlayer(String name,String color){
 
@@ -14,14 +14,14 @@ public class Game {
         //Resolve String:
         //set currFigure type: and color
         //Pass Information: Figur and Position: end to move
+        chessboard.isCheck;
         Move.move(figur,end);
-
 
     }
 
 
     public void tryMove(String move){
-       // Chessboard.checkMove(move);
+        ChessBoardoard.checkMove(move);
 
     }
     public boolean checkMate(){
--- Project 2/src/Move.java
+++ Project 2/src/Move.java
@@ -1,6 +1,7 @@
 public class Move {
 
     public void move(Player player, Figur figur, Coordinates end){
+
      //   currX = Figur.Coordinates.getX();
      //   currY = Figur.Coordinates.getY();
      //   if(Chessboard.move((currX,currY),end) == true){
