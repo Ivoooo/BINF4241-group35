5a07e863665222157e8218dfdb4948bdbcf335d7
MarcoUZH <56075874+MarcoUZH@users.noreply.github.com>
2019-10-22 13:53:56 +0200

    Checkmove queen update

--- Project 2/src/Queen.java
+++ Project 2/src/Queen.java
@@ -2,11 +2,43 @@ public class Queen extends Figure {
     public Queen(Attributes.colors color) {
         super(color, Attributes.types.queen);
     }
-    public boolean checkmove(int current_x, int current_y, int new_x, int new_y) {
-        if(new_x - current_x == new_y -current_y) return true;
-        if(new_x - current_x == 0 && new_y - current_y != 0) return true;
-        if(new_y - current_y == 0 && new_x- current_x != 0) return true;
-        else return false;
+    public boolean checkmove(int current_x, int current_y, int new_x, int new_y, Figure[][] board) {
+        if (new_x - current_x > 0) {
+            for (; current_x < new_x; ++current_x) {
+                if (new_y - current_y > 0) {
+                    current_y += 1;
+                    if (board[current_x][current_y] != null) return false;
+                }
+
+                if (new_y - current_y < 0) {
+                    current_y -= 1;
+                    if (board[current_x][current_y] != null) return false;
+
+
+                } else {
+                    if (board[current_x][current_y] != null) return false;
+                }
+            }
+        }
+        if (new_x - current_x < 0) {
+            for (; current_x > new_x; --current_x) {
+                if (new_y - current_y > 0) {
+                    current_y += 1;
+                    if (board[current_x][current_y] != null) return false;
+                    }
+                if (new_y - current_y < 0) {
+                    current_y -= 1;
+                    if (board[current_x][current_y] != null) return false;
+                }
+                else { if (board[current_x][current_y] != null) return false;
+                    }
+
+                }
+            }
+        if (new_x - current_x == new_y - current_y) return true;
+        if (new_x - current_x == 0 && new_y - current_y != 0) return true;
+        if (new_y - current_y == 0 && new_x - current_x != 0) return true;
+        return true;
 
     }
 }
