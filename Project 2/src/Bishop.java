7c76b37313d54df96b9fff8172bda8377a66bc2c
ivoooo <ivo.aeschlimann@hotmail.com>
2019-10-22 14:47:11 +0200

    fixed Bishop movement

--- Project 2/src/Bishop.java
+++ Project 2/src/Bishop.java
@@ -1,13 +1,14 @@
+import static java.lang.Math.abs;
+
 public class Bishop extends Figure {
     public Bishop(Attributes.colors color) {
         super(color, Attributes.types.bishop);
     }
 
     public boolean checkmove(int current_x, int current_y, int new_x, int new_y, Figure[][] board) {
-
-        if (!(new_x - current_x == new_y - current_y)) return false;
+        if (abs(new_x - current_x) != abs(new_y - current_y)) return false;
         if (new_x - current_x > 0) {
-            for (; current_x < new_x; ++current_x) {
+            for (++current_x; current_x < new_x; ++current_x) {
                 if (new_y - current_y > 0) {
                     current_y += 1;
                     if (board[current_x][current_y] != null) return false;
@@ -21,7 +22,7 @@ public class Bishop extends Figure {
             }
         }
         if (new_x - current_x < 0) {
-            for (; current_x > new_x; --current_x) {
+            for (--current_x; current_x > new_x; --current_x) {
                 if (new_y - current_y > 0) {
                     current_y += 1;
                         if (board[current_x][current_y] != null) return false;
