706c27ad73b8a47e39a624d8d994e876581f9417
MarcoUZH <56075874+MarcoUZH@users.noreply.github.com>
2019-10-22 14:25:19 +0200

    updatee

--- Project 2/src/Bishop.java
+++ Project 2/src/Bishop.java
@@ -1,12 +1,10 @@
-import java.lang.Math;
 public class Bishop extends Figure {
     public Bishop(Attributes.colors color) {
         super(color, Attributes.types.bishop);
     }
 
     public boolean checkmove(int current_x, int current_y, int new_x, int new_y, Figure[][] board) {
-        int xabsolut = Math.abs(new_x - current_x);
-        int yabsolut = Math.abs(new_y - current_y);
+
         if (!(new_x - current_x == new_y - current_y)) return false;
         if (new_x - current_x > 0) {
             for (; current_x < new_x; ++current_x) {
--- Project 2/src/Queen.java
+++ Project 2/src/Queen.java
@@ -36,6 +36,19 @@ public class Queen extends Figure {
 
                 }
             }
+        if(new_x - current_x == 0){
+            if(new_y - current_y > 0){
+                for(;current_y < new_y; ++current_y){
+                    if(board[current_x][current_y] != null) return false;
+                }
+            }
+            else{
+                for(;current_y > new_y; --current_y){
+                    if(board[current_x][current_y] != null) return false;
+                }
+
+            }
+        }
         if (new_x - current_x == new_y - current_y) return true;
         if (new_x - current_x == 0 && new_y - current_y != 0) return true;
         if (new_y - current_y == 0 && new_x - current_x != 0) return true;
