ea4849814e64d624b6410b5fb1073dcbabf4b24c
ivoooo <ivo.aeschlimann@hotmail.com>
2019-11-18 16:03:26 +0100

    Dishwasher mostly implemented

--- Project 4/src/Dishwasher.java
+++ Project 4/src/Dishwasher.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * This class implements Runnable. That means that you need to
  * implement the run() method to describe the Thread behaviour.
@@ -6,69 +8,60 @@
  * */
 
 public class Dishwasher implements Runnable{
-    private boolean running;    // represent the state of the thread
-    private int time = -1;           // represent the time of life of the thread
-    private String program;
-
-    public Dishwasher(){
-        running = false;
-    }
-
-    /**
-     * This method check if the thread is running
-     * @return the state of the thread
-     * */
+    private boolean isSwitchedOn = false;
+    private int time = -1;
+    private long timeStarted = -1;
+    private boolean running = false;
+    private Scanner input = new Scanner(System.in);
 
     public void on() {
-        return;
+        this.isSwitchedOn = true;
     }
 
     public void start() {
-        return;
+        this.run();
     }
 
     public void chooseProgram() {
-        return;
-    }
-
-    public void checkTimer() {
-        return;
-    }
-
-    public void stop() {
-        return;
-    }
-
-    public void off() {
-        return;
-    }
-
-    public boolean isRunning(){
-        return running;
-    }
-
-    /**
-     * This method contains the behaviour of the thread. Its implementation
-     * is mandatory. In this case, the thread will sleep for the amount of time
-     * specified in the constructor and handle the state of the Thread.
-     * */
-    @Override
-    public void run() {
-        try {
-            running = true;
-            Thread.sleep(time);
-            running = false;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        if(this.isSwitchedOn) {
+            String program = input.next();
+            if(program.equals("glasses")) this.time = 12000;
+            else if(program.equals("plates")) this.time = 15000;
+            else if(program.equals("pans")) this.time = 8000;
+            else if(program.equals("mixed")) this.time = 20000;
+            else this.time = 10000;
         }
     }
 
-    public void setProgram(String program) {
-        this.program = program;
-        if(program.equals("glasses")) this.time = 12000;
-        else if(program.equals("plates")) this.time = 15000;
-        else if(program.equals("pans")) this.time = 8000;
-        else if(program.equals("mixed")) this.time = 20000;
-        else this.time = 10000;
+    public void checkTimer() {
+        long tmp = timeStarted + time - System.currentTimeMillis();
+        if(this.isSwitchedOn && time != -1) System.out.println("Time remaining: " + tmp);
+    }
+
+    public void stop() {
+        if(this.isSwitchedOn) {
+            if(this.running) {
+                System.out.println("Sorry it's currently running");
+                return;
+            }
+            this.time = -1;
+        }
+    }
+
+    public void off() {
+        //todo check if possible
+        this.isSwitchedOn = false;
+    }
+
+    @Override
+    public void run() {
+        try {
+            this.running = true;
+            this.timeStarted = System.currentTimeMillis();
+            Thread.sleep(time);
+            this.running = false;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
