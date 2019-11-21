4715453ca35febad02673f24ba6c1be58d5dc091
ivoooo <ivo.aeschlimann@hotmail.com>
2019-11-21 15:26:00 +0100

    made variables private

--- Project 4/src/Smartphone.java
+++ Project 4/src/Smartphone.java
@@ -4,13 +4,13 @@ public class Smartphone {
     public String phoneCommand;
     private Command slot;
 
-    Scanner scanner = new Scanner(System.in);
-    int menuItem=0;
-    int menuItem2=0;
-    devices dev ;
-    boolean ovenON = false;
-    boolean microON = false;
-    boolean dishON  = false;
+    private Scanner scanner = new Scanner(System.in);
+    private int menuItem=0;
+    private int menuItem2=0;
+    private devices dev ;
+    private boolean ovenON = false;
+    private boolean microON = false;
+    private boolean dishON  = false;
 
     Oven oven = new Oven();
     Cleaningrobot robot = new Cleaningrobot();
